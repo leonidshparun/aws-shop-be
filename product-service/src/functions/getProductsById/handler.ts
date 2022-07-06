dd7c613dfc5b2a6d8526140d9c90adeb19fea976
Leanid Shparun <leanid_shparun@epam.com>
2022-07-06 12:06:48 +0400

    feat: add getProductsById lambda

---
+++ product-service/src/functions/getProductsById/handler.ts
@@ -0,0 +1,23 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+import { formatJSONResponse } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+
+import { getProductById } from '../../mock/mock-db';
+
+const getProductsById: ValidatedEventAPIGatewayProxyEvent = async (event) => {
+  const productId = event.pathParameters.productId;
+
+  try {
+    const product = await getProductById(productId);
+
+    return formatJSONResponse({
+      product,
+    });
+  } catch (error) {
+    return formatJSONResponse({
+      message: error.message
+    });
+  }
+};
+
+export const main = middyfy(getProductsById);
