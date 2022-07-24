c82a142e97e65ad47f3d3550655ca8d450b5ef2f
Leanid Shparun <leanid_shparun@epam.com>
2022-07-24 11:11:46 +0400

    feat: integrated product db service into getProductList

--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -1,18 +1,4 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { formatJSONResponse } from '@libs/api-gateway';
-import { middyfy } from '@libs/lambda';
+import { ProductDatabaseService } from '../../services/product-database.service';
+import { getProductsListHOF } from './lambda';
 
-import { getProductList } from '../../mock/mock-db';
-import schema from './schema';
-
-export const getProductsList: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => {
-  try {
-    const items = await getProductList();
-
-    return formatJSONResponse({ items });
-  } catch (error) {
-    return formatJSONResponse({ message: error.message }, 404);
-  }
-};
-
-export const main = middyfy(getProductsList);
+export const main = getProductsListHOF(ProductDatabaseService.create());
