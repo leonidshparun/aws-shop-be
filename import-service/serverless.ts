301b0880e3960b65a9dcfadb7d4a3e3b43cab104
Leanid Shparun <leanid_shparun@epam.com>
2022-07-28 15:59:10 +0400

    feat: import products file lambda implemented

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -1,35 +1,54 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 
-import hello from '@functions/hello';
+import importProductsFile from "@functions/importProductsFile";
 
 const serverlessConfiguration: AWS = {
-  service: 'import-service',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  service: "import-service",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild"],
+  useDotenv: true,
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
+    region: "eu-west-1",
+    stage: "dev",
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: ["s3:ListBucket"],
+        Resource: ["arn:aws:s3:::aws-shop-import-service"],
+      },
+      {
+        Effect: "Allow",
+        Action: ["s3:*"],
+        Resource: ["arn:aws:s3:::aws-shop-import-service/*"],
+      },
+    ],
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+      S3_IMPORT_BUCKET: "${env:S3_IMPORT_BUCKET}",
+      S3_UPLOAD_FOLDER: "${env:S3_UPLOAD_FOLDER}",
+      S3_SIGNED_URL_EXPIRE: "${env:S3_SIGNED_URL_EXPIRE}",
+      S3_IMPORT_CONTENT_TYPE: "${env:S3_IMPORT_CONTENT_TYPE}",
     },
   },
   // import the function via paths
-  functions: { hello },
+  functions: { importProductsFile },
   package: { individually: true },
   custom: {
     esbuild: {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
     },
   },
---
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -0,0 +1,38 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
+import { formatJSONResponse } from "@libs/api-gateway";
+import { middyfy } from "@libs/lambda";
+
+import { S3 } from "aws-sdk";
+
+import schema from "./schema";
+
+const importProductsFile: ValidatedEventAPIGatewayProxyEvent<
+  typeof schema
+> = async (event) => {
+  const {
+    S3_IMPORT_BUCKET,
+    S3_UPLOAD_FOLDER,
+    S3_SIGNED_URL_EXPIRE,
+    S3_IMPORT_CONTENT_TYPE,
+  } = process.env;
+
+  const fileName = event.queryStringParameters.name;
+
+  const s3 = new S3({ region: "eu-west-1" });
+  const catalogPath = `${S3_UPLOAD_FOLDER}/${fileName}`;
+
+  const params = {
+    Bucket: S3_IMPORT_BUCKET,
+    Key: catalogPath,
+    Expires: parseInt(S3_SIGNED_URL_EXPIRE, 10),
+    ContentType: S3_IMPORT_CONTENT_TYPE,
+  };
+
+  const url = s3.getSignedUrl("putObject", params);
+
+  return formatJSONResponse({
+    url,
+  });
+};
+
+export const main = middyfy(importProductsFile);
